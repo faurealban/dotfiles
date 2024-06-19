487b518cbd66a88ead4f1d50d8200a19eed66390
faurealban <faurealban@proton.me>
2024-06-19 22:54:08 +0200

    added rust-analyzer to LSPs of nvim

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,10 +1,10 @@
--- EXAMPLE 
+-- EXAMPLE
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "pyright", "rust_analyzer" }
 
 -- lsps with default configs
 for _, lsp in ipairs(servers) do
@@ -14,18 +14,3 @@ for _, lsp in ipairs(servers) do
         capabilities = capabilities,
     }
 end
-
--- typescript
-lspconfig.tsserver.setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-}
-
--- python
-lspconfig.pyright.setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities =  capabilities,
-}
-
