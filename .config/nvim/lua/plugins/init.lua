14d8d886ef417426ede926985dea388c0d719230
faurealban <faurealban@proton.me>
2024-07-11 13:47:31 +0200

    added obsidian bind

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -44,4 +44,13 @@ return {
             },
         },
     },
+
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+    },
 }
