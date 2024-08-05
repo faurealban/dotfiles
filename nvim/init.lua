d5c6f8b035a9681da4625f1291ce4bc6d6b653e6
faurealban <alban.faure@proton.me>
2024-08-05 13:17:53 +0200

    added nvim peek plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -51,3 +51,16 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
         }
     end,
 })
+
+-- peek
+require("peek").setup {
+    auto_load = true,
+    close_on_bdelete = true,
+    syntax = true,
+    theme = "dark",
+    update_on_change = true,
+    app = "firefox",
+    filetype = { "markdown" },
+    throttle_at = 200000,
+    throttle_time = "auto",
+}
