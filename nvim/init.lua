763da2fea813dbd5a24db80e61368a716289b0e6
faurealban <alban.faure@proton.me>
2024-08-05 13:39:54 +0200

    updated peek plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -59,7 +59,7 @@ require("peek").setup {
     syntax = true,
     theme = "dark",
     update_on_change = true,
-    app = "firefox",
+    app = { "firefox", "-new-window" },
     filetype = { "markdown" },
     throttle_at = 200000,
     throttle_time = "auto",
