9f157e0e28ddaa26937eeaccf6c1e6534f65f686
faurealban <alban.faure@proton.me>
2024-09-30 02:33:29 +0200

    fix: zsh alias 'ctf' now opening nvim

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -19,7 +19,7 @@ alias push="git push"
 alias dots="cd ~/gits/dotfiles/ && nvim"
 alias conf="cd ~/.config/ && nvim"
 alias o="cd ~/gits/project-o/ && nvim"
-alias ctf="cd ~/gits/ctf"
+alias ctf="cd ~/gits/ctf && nvim"
 
 alias ath="~/.config/scripts/ui/change_theme.zsh"
 alias bat="cat /sys/class/power_supply/BAT1/capacity"
