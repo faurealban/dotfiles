97fd2ec184ae90255cf22dee5fbc2bbda7fb1658
faurealban <alban.faure@proton.me>
2024-10-02 02:45:26 +0200

    fix: sudo commands

--- scripts/maintenance/monthly.zsh
+++ scripts/maintenance/monthly.zsh
@@ -1,13 +1,19 @@
 #!/bin/zsh
 
+# Update system
+sudo pacman -Syu
+
 # Update mirrors
-reflector --protocol https --verbose --latest 25 --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector --protocol https --verbose --latest 25 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Update system
 sudo pacman -Syu
 
 # Clean journal
-journalctl --vacuum-time=4weeks
+sudo journalctl --vacuum-time=4weeks
 
 # Clean pacman cache
-paccache -ruk0
+sudo paccache -ruk0
+
+# Update system
+sudo pacman -Syu
